cc7424c206b7caa5a6edeb6fe93917714d1ab4af
Ahsan-1015 <ahshanh964@gmail.com>
2024-12-08 02:14:25 +0600

    Vercel deploy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -13,9 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khjiv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 // MongoDB connection URI from environment variables
-const uri =
-  'mongodb+srv://aaaaahshanhabib:b2zPqQIaxDArXuON@cluster0.ing8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ing8w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient
 const client = new MongoClient(uri, {
@@ -28,7 +29,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const database = client.db('campaignDb');
     const campaignCollection = database.collection('campaigns');
@@ -37,7 +38,9 @@ async function run() {
     // Get all campaigns
     app.get('/campaigns', async (req, res) => {
       try {
-        const cursor = campaignCollection.find();
+        // Sort campaigns by createdAt field (most recent first)
+        const cursor = campaignCollection.find().sort({ createdAt: -1 }); // -1 for descending order (latest first)
+
         const campaigns = await cursor.toArray();
         res.status(200).json(campaigns);
       } catch (error) {
@@ -104,12 +107,18 @@ async function run() {
         return res.status(400).json({ message: 'User email is required.' });
       }
 
+      // Add createdAt field to the campaign data
+      const newCampaign = {
+        ...data,
+        createdAt: new Date(), // Current timestamp
+      };
+
       try {
-        const result = await campaignCollection.insertOne(data);
+        const result = await campaignCollection.insertOne(newCampaign);
         res.status(201).json(result);
       } catch (error) {
-        console.error('Error adding visa:', error);
-        res.status(500).json({ message: 'Error adding visa.' });
+        console.error('Error adding campaign:', error);
+        res.status(500).json({ message: 'Error adding campaign.' });
       }
     });
 
